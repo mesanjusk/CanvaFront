3978d825a0b84033551c61f44ea679a15ae08bf0
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-09-28 20:32:33 +0530

    Update TemplateCard.jsx

--- src/components/TemplateCard.jsx
+++ src/components/TemplateCard.jsx
@@ -1,9 +1,21 @@
-import { memo } from "react";
-import { extractTemplateSize, pickTemplatePreview } from "../utils/templateUtils";
+import { memo } from 'react';
 
-const TemplateCard = memo(({ template, onSelect }) => {
-
-  );
-});
+const TemplateCard = memo(({ template, onSelect }) => (
+  <div
+    className="cursor-pointer rounded overflow-hidden shadow hover:shadow-lg bg-white dark:bg-gray-700"
+    onClick={() => onSelect(template.layout ? JSON.parse(template.layout) : template.data)}
+  >
+    {template.image && (
+      <img
+        src={template.image}
+        alt={template.title || template.name}
+        className="w-full h-24 object-cover"
+      />
+    )}
+    <div className="p-2 text-center text-sm">
+      {template.title || template.name}
+    </div>
+  </div>
+));
 
 export default TemplateCard;

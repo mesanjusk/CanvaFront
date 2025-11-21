a87dcbce0050b062d2e58b4a6ee187f292cde875
sbframee <104187401+sbframee@users.noreply.github.com>
2025-11-21 15:12:50 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,12 @@ import Dashboard from './pages/Dashboard';
 import User from './pages/User';
 import Login from './components/Login';
 import ImageUploader from './components/ImageUploader';
-import Enquiry from './pages/Enquiry';
 import Courses from './pages/Courses';
-import Batches from './pages/Batches';
 import Signup from './components/Signup';
-import OrgCategories from './pages/OrgCategories';
-import Education from './pages/Education';
-import Exam from './pages/Exam';
-import PaymentMode from './pages/PaymentMode';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
-import InstituteProfile from './pages/instituteProfile';
-import Owner from './pages/Owner';
 import PrivateRoute from './components/PrivateRoute';
-import CoursesCategory from './pages/CoursesCategory';
-import Leads from './reports/Leads';
-import AllAdmission from './reports/allAdmission';
-import AddLead from './pages/AddLead';
-import AddNew from './components/admissions/AddAdmission';
-import Followup from './pages/Followup';
-import WhatsAppAdminPage from './pages/WhatsAppAdminPage';
-import AddReciept from './pages/addReciept';
-import AddPayment from './pages/addPayment';
-import AllLeadByAdmission from './reports/allLeadByAdmission';
-import AddAttendance from './pages/AddAttendance';
-import AllAttendance from './reports/allAttendance';
-import AllBatches from './reports/allBatches';
-import AllBalance from './reports/allBalance';
-import AddAccount from './pages/AddAccount';
-import AllExams from './reports/allExams';
-import Institutes from './pages/Institutes';
 import Students from './pages/Students';
-import Fees from './pages/Fees';
-import ToolsPanel from './pages/ToolsPanel';
 import CanvasEditor from './components/CanvasEditor';
 import ErrorBoundary from './components/ErrorBoundary';
 import TemplateGallery from './pages/TemplateGallery';
@@ -45,9 +18,6 @@ import TemplateManager from './pages/TemplateManager';
 import AddTemplate from './Pages/addTemplate';
 import AddCategory from './pages/addCategory';
 import AddSubcategory from './pages/addSubcategory';
-import BulkGenerator from './Pages/BulkGenerator';
-import VideoEditor from './pages/VideoEditor';
-import PrintLayout from './pages/PrintLayout';
 import CanvaHome from './Pages/canvaHome';
 import Subcategory from './Pages/Subcategory';
 import AddGallary from './Pages/addGallary';
@@ -76,37 +46,9 @@ export default function App() {
       <Route path="/:username" element={<PrivateRoute><DashboardLayout /></PrivateRoute>} >
         <Route index element={<Dashboard />} />
         <Route path="user" element={<User />} />
-        <Route path="batches" element={<Batches />} />
-        <Route path="enquiry" element={<Enquiry />} />
+       
         <Route path="courses" element={<Courses />} />
         <Route path="students" element={<Students />} />
-        <Route path="orgcategories" element={<OrgCategories />} />
-        <Route path="education" element={<Education />} />
-        <Route path="exam" element={<Exam />} />
-        <Route path="paymentmode" element={<PaymentMode />} />
-        <Route path="instituteProfile" element={<InstituteProfile />} />
-        <Route path="owner" element={<Owner />} />
-        <Route path="institutes" element={<Institutes />} />
-        <Route path="coursesCategory" element={<CoursesCategory />} />
-        <Route path="leads" element={<Leads />} />
-        <Route path="allAdmission" element={<AllAdmission />} />
-        <Route path="allLeadByAdmission" element={<AllLeadByAdmission />} />
-        <Route path="add-lead" element={<AddLead />} />
-        <Route path="addNewAdd" element={<AddNew />} />
-        <Route path="addReciept" element={<AddReciept />} />
-        <Route path="addPayment" element={<AddPayment />} />
-        <Route path="addAccount" element={<AddAccount />} />
-        <Route path="followup" element={<Followup />} />
-        <Route path="addAttendance" element={<AddAttendance />} />
-        <Route path="allAttendance" element={<AllAttendance />} />
-        <Route path="allBalance" element={<AllBalance />} />
-        <Route path="allBatches" element={<AllBatches />} />
-        <Route path="whatsapp" element={<WhatsAppAdminPage />} />
-        <Route path="allExams" element={<AllExams />} />
-        <Route path="fees" element={<Fees />} />
-        <Route path="tools" element={<ToolsPanel />} />
-        <Route path="video" element={<VideoEditor />} />
-        <Route path="print-layout" element={<PrintLayout />} />
         
           <Route path="Gallary" element={<AddGallary />} />
           <Route path="addTemplate" element={<AddTemplate />} />
@@ -114,8 +56,6 @@ export default function App() {
           <Route path="addSubcategory" element={<AddSubcategory />} />
       <Route path="templateManager" element={<TemplateManager />} />
       </Route>
-
-      <Route path="/:username/bulk" element={<PrivateRoute><BulkGenerator /></PrivateRoute>} />
 
       {/* 🧭 Fallback */}
       <Route path="*" element={<Navigate to="/" replace />} />
--- src/reports/AllTransaction.jsx
+++ src/reports/AllTransaction.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import BASE_URL from '../config';
-import TopNavbar from '../Pages/topNavbar';
-import Footer from '../Pages/footer';
 import { FaWhatsapp, FaSortUp, FaSortDown } from 'react-icons/fa';
 import * as XLSX from 'xlsx';
 import { jsPDF } from "jspdf";
@@ -158,7 +156,7 @@ const AllTransaction = () => {
     return (
         <>
             <div className="no-print">
-                <TopNavbar />
+               
             </div>
             <div className="pt-12 pb-20">
                 <div className="mt-6 max-w-7xl mx-auto  p-4">

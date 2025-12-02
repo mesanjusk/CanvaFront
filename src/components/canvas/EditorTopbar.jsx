27da424ec7a5fd2763c457f8eda50da4c4770317
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-12-02 11:53:51 +0530

    Handle template timeouts without surfacing errors

--- src/components/canvas/EditorTopbar.jsx
+++ src/components/canvas/EditorTopbar.jsx
@@ -19,87 +19,108 @@ import {
   ZoomOut,
   Undo,
   Redo,
-  HelpOutline
+  HelpOutline,
+  Tune
 } from "@mui/icons-material";
 
 
 export function EditorTopBar({
-fileName = 'Untitled',
-zoom = 100,
-onZoomChange = () => {},
-onSave = () => {},
-onDownload = () => {},
-onUndo = () => {},
-onRedo = () => {},
-onToggleHelp = () => {},
-showGrid = false,
-onToggleGrid = () => {},
-snapObjects = true,
-onToggleSnap = () => {},
+  fileName = "Untitled",
+  zoom = 100,
+  onZoomChange = () => {},
+  onSave = () => {},
+  onDownload = () => {},
+  onUndo = () => {},
+  onRedo = () => {},
+  onToggleHelp = () => {},
+  showGrid = false,
+  onToggleGrid = () => {},
+  snapObjects = true,
+  onToggleSnap = () => {},
+  onToggleLeftPanel = () => {},
+  onToggleRightPanel = () => {},
+  isMobile = false,
 }) {
-return (
-<AppBar position="static" color="default" elevation={1}>
-<Toolbar className="flex items-center gap-3">
-<IconButton edge="start" aria-label="menu" size="large"><MenuIcon /></IconButton>
-<Typography variant="subtitle1" className="truncate max-w-xs">{fileName}</Typography>
+  return (
+    <AppBar position="static" color="default" elevation={1}>
+      <Toolbar className="flex items-center gap-3">
+        <IconButton edge="start" aria-label="menu" size="large" onClick={isMobile ? onToggleLeftPanel : undefined}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="subtitle1" className="truncate max-w-xs">
+          {fileName}
+        </Typography>
 
+        <div className="ml-4 flex items-center gap-2">
+          <Button startIcon={<Save />} onClick={onSave} variant="outlined" size="small">
+            Save
+          </Button>
+          <Button startIcon={<Download />} onClick={onDownload} variant="contained" size="small">
+            Export
+          </Button>
+        </div>
 
-<div className="ml-4 flex items-center gap-2">
-<Button startIcon={<Save />} onClick={onSave} variant="outlined" size="small">Save</Button>
-<Button startIcon={<Download />} onClick={onDownload} variant="contained" size="small">Export</Button>
-</div>
+        <div className="flex-1" />
+        <div className="flex items-center gap-3">
+          <IconButton onClick={onUndo} size="large" title="Undo">
+            <Undo />
+          </IconButton>
+          <IconButton onClick={onRedo} size="large" title="Redo">
+            <Redo />
+          </IconButton>
 
+          <div className="flex items-center gap-2 px-3">
+            <ZoomOut />
+            <Slider
+              value={zoom}
+              onChange={(e, v) => onZoomChange(v)}
+              min={10}
+              max={400}
+              sx={{ width: 160 }}
+            />
+            <ZoomIn />
+          </div>
 
-<div className="flex-1" />
-<div className="flex items-center gap-3">
-<IconButton onClick={onUndo} size="large" title="Undo"><Undo /></IconButton>
-<IconButton onClick={onRedo} size="large" title="Redo"><Redo /></IconButton>
+          <FormControlLabel
+            control={<Switch checked={showGrid} onChange={(e) => onToggleGrid(e.target.checked)} size="small" />}
+            label="Grid"
+          />
 
+          <FormControlLabel
+            control={<Switch checked={snapObjects} onChange={(e) => onToggleSnap(e.target.checked)} size="small" />}
+            label="Snap"
+          />
 
-<div className="flex items-center gap-2 px-3">
-<ZoomOut />
-<Slider
-value={zoom}
-onChange={(e, v) => onZoomChange(v)}
-min={10}
-max={400}
-sx={{ width: 160 }}
-/>
-<ZoomIn />
-</div>
+          {isMobile && (
+            <IconButton onClick={onToggleRightPanel} size="large" title="Inspector">
+              <Tune />
+            </IconButton>
+          )}
 
-
-<FormControlLabel
-control={<Switch checked={showGrid} onChange={(e) => onToggleGrid(e.target.checked)} size="small" />}
-label="Grid"
-/>
-
-
-<FormControlLabel
-control={<Switch checked={snapObjects} onChange={(e) => onToggleSnap(e.target.checked)} size="small" />}
-label="Snap"
-/>
-
-
-<IconButton onClick={onToggleHelp} title="Shortcuts" size="large"><HelpOutline /></IconButton>
-</div>
-</Toolbar>
-</AppBar>
-);
+          <IconButton onClick={onToggleHelp} title="Shortcuts" size="large">
+            <HelpOutline />
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 }
 EditorTopBar.propTypes = {
-fileName: PropTypes.string,
-zoom: PropTypes.number,
-onZoomChange: PropTypes.func,
-onSave: PropTypes.func,
-onDownload: PropTypes.func,
-onUndo: PropTypes.func,
-onRedo: PropTypes.func,
-onToggleHelp: PropTypes.func,
-showGrid: PropTypes.bool,
-onToggleGrid: PropTypes.func,
-snapObjects: PropTypes.bool,
-onToggleSnap: PropTypes.func,
+  fileName: PropTypes.string,
+  zoom: PropTypes.number,
+  onZoomChange: PropTypes.func,
+  onSave: PropTypes.func,
+  onDownload: PropTypes.func,
+  onUndo: PropTypes.func,
+  onRedo: PropTypes.func,
+  onToggleHelp: PropTypes.func,
+  showGrid: PropTypes.bool,
+  onToggleGrid: PropTypes.func,
+  snapObjects: PropTypes.bool,
+  onToggleSnap: PropTypes.func,
+  onToggleLeftPanel: PropTypes.func,
+  onToggleRightPanel: PropTypes.func,
+  isMobile: PropTypes.bool,
 };
 
 export default EditorTopBar;
